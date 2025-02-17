4bbc78676882241a5899ca2e453707732f292054
surya808493 <surajkumar75419696@gmail.com>
2025-02-17 14:47:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,10 +3,10 @@ import os
 
 
 # Required Variables Config
-API_ID = int(os.environ.get("API_ID", ""))
-API_HASH = os.environ.get("API_HASH", "")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-ADMIN = int(os.environ.get("ADMIN", ""))
+API_ID = int(os.environ.get("API_ID", "26992030"))
+API_HASH = os.environ.get("API_HASH", "4da7d71c6bc4512a886e41aca83a5ee3")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7781040017:AAEo-zuKXfEBzHYpoaJ7CPtPyH0PVoNWqiI")
+ADMIN = int(os.environ.get("ADMIN", "7406982863"))
 
 
 # Premium 4GB Renaming Client Config
@@ -14,13 +14,13 @@ STRING_SESSION = os.environ.get("STRING_SESSION", "")
 
 
 # Log & Force Channel Config
-FORCE_SUBS = os.environ.get("FORCE_SUBS", "")
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+FORCE_SUBS = os.environ.get("FORCE_SUBS", "-1002350746988")
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002402091090"))
 
 
 # Mongo DB Database Config
-DATABASE_URL = os.environ.get("DATABASE_URL", "")
-DATABASE_NAME = os.environ.get("DATABASE_NAME", "TechifyBots")
+DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://sunil664023:eieqy2jRVIbzD29r@cluster0.ezot5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
 
 
 # Other Variables Config
